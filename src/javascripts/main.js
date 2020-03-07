17a83af0031084df751656a62b4736cfb4d891b0
Aaron Lain <aaron.j.lain@gmail.com>
2020-03-07 12:51:29 -0600

    buttons

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,12 +1,14 @@
 import students from './helpers/data/studentData';
 import navbar from './components/navbar/navbar';
 import list from './components/studentList/studentList';
+import houseButtonGroup from './components/houseButtonGroup/houseButtonGroup';
 import '../styles/main.scss';
 
 const init = () => {
   const allStudents = students.getStudents();
   navbar.loadNavbar();
   list.createStudentList(allStudents);
+  houseButtonGroup.createHouseButtonGroup();
 };
 
 init();

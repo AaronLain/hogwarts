f2e8af0580da4f71ebe99175aa1a2bdf757a5740
Aaron Lain <aaron.j.lain@gmail.com>
2020-03-07 11:55:40 -0600

    colors and more sass configuration

--- src/javascripts/components/studentList/studentList.js
+++ src/javascripts/components/studentList/studentList.js
@@ -1,11 +1,14 @@
+import house from '../../helpers/data/houseData';
 import studentCard from '../studentCard/studentCard';
 import utils from '../../helpers/utils';
 import './studentList.scss';
 
 const createStudentList = (students) => {
+  const houses = house.getHouses();
   let domString = '<ul class="student-list">';
   students.forEach((student) => {
-    domString += studentCard.createStudentCard(student);
+    const studentHouse = houses.find((x) => x.id === student.houseId);
+    domString += studentCard.createStudentCard(student, studentHouse);
   });
   domString += '</ul>';
   utils.printToDom('student-container', domString);

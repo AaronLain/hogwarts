11d8083e55f5e0c85c75b1dd22f032f22cef1a32
Aaron Lain <aaron.j.lain@gmail.com>
2020-03-07 10:50:50 -0600

    added student list

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,8 +1,12 @@
+import students from './helpers/data/studentData';
 import navbar from './components/navbar/navbar';
+import list from './components/studentList/studentList';
 import '../styles/main.scss';
 
 const init = () => {
+  const allStudents = students.getStudents();
   navbar.loadNavbar();
+  list.createStudentList(allStudents);
 };
 
 init();
